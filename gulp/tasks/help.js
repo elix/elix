eeca7c492432049fb2a8ca12b07bddfa6a2fa0dc
Jan Miksovsky <jan@quickui.org>
2016-12-05 13:15:31 -0800

    Simplify `gulp help` definition and output.

--- gulp/tasks/help.js
+++ gulp/tasks/help.js
@@ -1,22 +1,23 @@
 /*jslint node: true */
 'use strict';
 
-const gulp = require('gulp');
 const gutil = require('gulp-util');
 
 //
 // Help task - prints to the console the tasks that are available to be run from the command line
 //
-const helpTask = function() {
-  gutil.log('gulp commands this project supports:');
-  gutil.log('  gulp build (builds consolidated elix-all.js, all package distributions, all documentation, and all tests)');
-  gutil.log('  gulp devbuild (same as build minus building the documentation)');
-  gutil.log('  gulp docs (builds all packages README.md files)');
-  gutil.log('  gulp lint (runs jshint on all .js files)');
-  //gutil.log('  gulp npm-publish:package-name|* (publishes packages/package-name or all packages (packages/*) to npm)');
-  //gutil.log('  gulp set-version:version (updates package.json version values and dependencies. Ex: gulp set-version:1.0.30)');
-  //gutil.log('  gulp saucelabs (Runs SauceLabs tests. You must set environment variables SAUCE_USERNAME and SAUCE_ACCESS_KEY.)');
-  gutil.log('  gulp watch (builds and watches changes to project files)');
+function helpTask() {
+  gutil.log(`gulp commands this project supports:
+
+    gulp build (builds all packages, docs, and tests)
+    gulp devbuild (same as build minus building the documentation)
+    gulp docs (builds all packages README.md files)
+    gulp lint (runs jshint on all .js files)
+    gulp watch (builds and watches changes to project files)
+  `);
+  //  gulp npm-publish:package-name|* (publishes packages/package-name or all packages (packages/*) to npm)
+  //  gulp set-version:version (updates package.json version values and dependencies. Ex: gulp set-version:1.0.30)
+  //  gulp saucelabs (Runs SauceLabs tests. You must set environment variables SAUCE_USERNAME and SAUCE_ACCESS_KEY.)
 }
 
 module.exports = helpTask;
