87bf18d1efccc7e62e83004944707aa14ffeefd6
Jan Miksovsky <jan@quickui.org>
2017-06-28 11:32:52 -0700

    Remove old code.

--- mixins/DialogModalityMixin.js
+++ mixins/DialogModalityMixin.js
@@ -3,8 +3,8 @@ import symbols from './symbols.js';
 
 
 // Symbols for private data members on an element.
-const previousBodyStyleOverflow = Symbol('previousBodyStyleOverflow');
-const previousDocumentMarginRight = Symbol('previousDocumentMarginRight');
+const previousBodyOverflowKey = Symbol('previousBodyStyleOverflow');
+const previousDocumentMarginRightKey = Symbol('previousDocumentMarginRight');
 
 
 // Expects: keydown, opened, backdrop, shadowCreated
@@ -17,8 +17,8 @@ export default function DialogModalityMixin(Base) {
       if (super[symbols.afterEffect]) { super[symbols.afterEffect](effect); }
       if (effect === 'closing') {
         // Restore body's previous degree of scrollability.
-        document.body.style.overflow = this[previousBodyStyleOverflow];
-        document.documentElement.style.marginRight = this[previousDocumentMarginRight];
+        document.body.style.overflow = this[previousBodyOverflowKey];
+        document.documentElement.style.marginRight = this[previousDocumentMarginRightKey];
       }
     }
 
@@ -27,18 +27,13 @@ export default function DialogModalityMixin(Base) {
       if (effect === 'opening') {
         // Mark body as non-scrollable, to absorb space bar keypresses and other
         // means of scrolling the top-level document.
-        // HACK: This tries to play with the document margin to minimize page
-        // reflow. This is just an experiment to get a feel for how awful this
-        // would be.
-        // TODO: Walk up the dialog's parent hierarchy and do the same for any
-        // scrollable parents in it.
         const scrollBarWidth = window.innerWidth - document.body.clientWidth;
-        this[previousBodyStyleOverflow] = document.body.style.overflow;
-        this[previousDocumentMarginRight] = scrollBarWidth > 0 ?
+        this[previousBodyOverflowKey] = document.body.style.overflow;
+        this[previousDocumentMarginRightKey] = scrollBarWidth > 0 ?
           document.documentElement.style.marginRight :
           null;
         document.body.style.overflow = 'hidden';
-        if (scrollBarWidth) {
+        if (scrollBarWidth > 0) {
           document.documentElement.style.marginRight = `${scrollBarWidth}px`;
         }
       }
@@ -69,52 +64,12 @@ export default function DialogModalityMixin(Base) {
           this.close();
           handled = true;
           break;
-
-        // case 32: // Space
-        // case 33: // Page Up
-        // case 34: // Page Down
-        // case 35: // End
-        // case 36: // Home
-        // case 37: // Left
-        // case 38: // Up
-        // case 39: // Right
-        // case 40: // Down
-        //   // Disable bubbling keyboard events that could potentially scroll
-        //   // the page.
-        //   if (event.target === this) {
-        //     disableEvent(event);
-        //     handled = true;
-        //   }
-        //   break;
       }
 
       // Prefer mixin result if it's defined, otherwise use base result.
       return handled || (super[symbols.keydown] && super[symbols.keydown](event)) || false;
     }
-
-    // [symbols.shadowCreated]() {
-    //   if (super[symbols.shadowCreated]) { super[symbols.shadowCreated](); }
-    //
-    //   // Disable scrolling of the background while dialog is open.
-    //   const backdrop = this.backdrop;
-    //   if (!backdrop) {
-    //     console.warn('DialogModalityMixin expects a component to define an "backdrop" property.');
-    //   }
-    //   backdrop.addEventListener('mousewheel', event => disableEvent(event));
-    //   backdrop.addEventListener('touchmove', event => {
-    //     // Don't disable multi-touch gestures like pinch-zoom.
-    //     if (event.touches.length === 1) {
-    //       disableEvent(event);
-    //     }
-    //   });
-    // }
   }
 
   return DialogModality;
 }
-
-
-// function disableEvent(event) {
-//   event.preventDefault();
-//   event.stopPropagation();
-// }
