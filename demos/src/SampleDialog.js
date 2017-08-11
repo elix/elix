2b43e443e71c96a98b2c780cb766a2be350952f4
Jan Miksovsky <jan@quickui.org>
2017-08-11 16:56:33 -0700

    Comments for sample dialog

--- demos/src/SampleDialog.js
+++ demos/src/SampleDialog.js
@@ -6,12 +6,15 @@ class SampleDialog extends Dialog {
 
   [symbols.shadowCreated]() {
     if (super[symbols.shadowCreated]) { super[symbols.shadowCreated](); }
+    // Have the dialog close itself when the user clicks anywhere within it. In
+    // many cases, you'll want to have a button ("OK", "Close", etc.) that
+    // performs this action.
     this.shadowRoot.querySelector('#overlayContent').addEventListener('click', () => {
       this.close('OK');
     });
   }
 
-  [symbols.template](filler) {
+  [symbols.template]() {
     return super[symbols.template](`
       <style>
         #message {
@@ -19,7 +22,7 @@ class SampleDialog extends Dialog {
         }
       </style>
       <div id="message">
-        ${filler || `<slot></slot>`}
+        <slot></slot>
       </div>
     `);
   }
