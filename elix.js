261ac99e22fdc8705918dad426f22a42e2cf20f2
Jan Miksovsky <jan@quickui.org>
2017-11-01 10:26:20 -0700

    Add missing elements to top-level elix.js.

--- elix.js
+++ elix.js
@@ -1,83 +1,56 @@
 /*
  * The complete set of Elix elements.
  * 
- * This is primarily used during testing, as it causes all Elix's elements and
- * mixins to be loaded/built. Generally, you can ignore this file, and import
- * the source file(s) you want from the src folder.
+ * This is primarily used during testing, as it causes all Elix's elements to be
+ * loaded/built. Generally, you can ignore this file, and import the source
+ * file(s) you want from the src folder.
  */
 
 
- // Import mixins
-import * as content from './mixins/content.js';
-import AttributeMarshallingMixin from './mixins/AttributeMarshallingMixin.js';
-import ClickSelectionMixin from './mixins/ClickSelectionMixin.js';
-import constants from './mixins/constants.js';
-import ContentItemsMixin from './mixins/ContentItemsMixin.js';
-import defaultScrollTarget from './mixins/defaultScrollTarget.js';
-import DirectionSelectionMixin from './mixins/DirectionSelectionMixin.js';
-import KeyboardDirectionMixin from './mixins/KeyboardDirectionMixin.js';
-import KeyboardMixin from './mixins/KeyboardMixin.js';
-import KeyboardPagedSelectionMixin from './mixins/KeyboardPagedSelectionMixin.js';
-import KeyboardPrefixSelectionMixin from './mixins/KeyboardPrefixSelectionMixin.js';
-import OpenCloseTransitionMixin from './mixins/OpenCloseTransitionMixin.js';
-import OverlayMixin from './mixins/OverlayMixin.js';
-import SelectionAriaMixin from './mixins/SelectionAriaMixin.js';
-import SelectionInViewMixin from './mixins/SelectionInViewMixin.js';
-import ShadowReferencesMixin from './mixins/ShadowReferencesMixin.js';
-import ShadowTemplateMixin from './mixins/ShadowTemplateMixin.js';
-import SingleSelectionMixin from './mixins/SingleSelectionMixin.js';
-import SlotContentMixin from './mixins/SlotContentMixin.js';
-import Symbol from './mixins/Symbol.js';
-import symbols from './mixins/symbols.js';
-
 // Import elements
 import AlertDialog from './elements/AlertDialog.js';
+import ArrowSelectionButton from './elements/ArrowSelectionButton.js';
 import AutosizeTextarea from './elements/AutosizeTextarea.js';
 import Dialog from './elements/Dialog.js';
 import Drawer from './elements/Drawer.js';
+import ElementBase from './elements/ElementBase.js';
 import ListBox from './elements/ListBox.js';
+import ModalBackdrop from './elements/ModalBackdrop.js';
 import Modes from './elements/Modes.js';
+import PageDot from './elements/PageDot.js';
 import Popup from './elements/Popup.js';
+import SlidingCarousel from './elements/SlidingCarousel.js';
+import SlidingPages from './elements/SlidingPages.js';
+import SlidingViewport from './elements/SlidingViewport.js';
 import Spread from './elements/Spread.js';
 import TabButton from './elements/TabButton.js';
 import Tabs from './elements/Tabs.js';
 import TabStrip from './elements/TabStrip.js';
 import Toast from './elements/Toast.js';
+import WrappedStandardElement from './elements/WrappedStandardElement.js';
 
 
 // The complete list of all mixins and elements.
 export default {
   AlertDialog,
-  AttributeMarshallingMixin,
+  ArrowSelectionButton,
   AutosizeTextarea,
-  ClickSelectionMixin,
-  constants,
-  content,
-  ContentItemsMixin,
-  defaultScrollTarget,
   Dialog,
-  DirectionSelectionMixin,
   Drawer,
-  KeyboardDirectionMixin,
-  KeyboardMixin,
-  KeyboardPagedSelectionMixin,
-  KeyboardPrefixSelectionMixin,
+  ElementBase,
   ListBox,
+  ModalBackdrop,
   Modes,
-  OpenCloseTransitionMixin,
-  OverlayMixin,
+  PageDot,
   Popup,
-  SelectionAriaMixin,
-  SelectionInViewMixin,
-  ShadowReferencesMixin,
-  ShadowTemplateMixin,
-  SingleSelectionMixin,
-  SlotContentMixin,
+  SlidingCarousel,
+  SlidingPages,
+  SlidingViewport,
   Spread,
   Symbol,
-  symbols,
   TabButton,
   Tabs,
   TabStrip,
-  Toast
+  Toast,
+  WrappedStandardElement
 };
