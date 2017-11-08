cef850cce3c19ccd32dce93dd1a918170b899baa
Jan Miksovsky <jan@quickui.org>
2017-11-08 15:51:01 -0800

    Type definition for HoverMixin.

---
+++ mixins/HoverMixin.d.ts
@@ -0,0 +1,10 @@
+// Elix is a JavaScript project, but we use TypeScript as an internal tool to
+// confirm our code is type safe.
+
+/// <reference path="../utilities/shared.d.ts"/>
+
+declare const HoverMixin: StateMixin<{}, {}, {}, {
+  hover: boolean;
+}>;
+
+export default HoverMixin;
