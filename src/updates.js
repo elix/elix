826afae3026628dd00360b49629eae989e3eae12
Jan Miksovsky <jan@quickui.org>
2018-01-08 16:44:19 -0800

    jsDoc comments

--- src/updates.js
+++ src/updates.js
@@ -2,6 +2,19 @@
  * Helpers for getting and setting component attributes and properties using
  * JavaScript dictionary objects.
  * 
+ * The `Element` prototype exposes several properties that expose a collection
+ * that can be modified but not set directly: attributes, childNodes, classList,
+ * and (for HTMLElement and SVGElement) style. Although these properties cannot
+ * be directly set (you can't write element.attributes = newAttributes), we can
+ * define simple and useful write semantics for updating these properties in
+ * bulk.
+ * 
+ * These helpers, especially [apply](#apply), are designed to let components
+ * declare a bulk set of updates they would like to apply to themselves or their
+ * elements in their shadow trees. Both [RenderUpdatesMixin](RenderUpdatesMixin)
+ * and [ContentItemsMixin](ContentItemsMixin) use the same dictionary format
+ * supported by `apply`.
+ * 
  * @module updates
  */
 
@@ -31,19 +44,60 @@ const previousChildNodesKey = Symbol('previousChildNodes');
 
 
 /**
- * @param {Element} element 
- * @param {object} props
+ * Apply bulk updates to an element's attributes, child nodes, classes, styles,
+ * and shadow elements.
+ * 
+ * This takes an `updates` dictionary. Certain keys of this dictionary are
+ * handled specially:
+ * 
+ * * `attributes`: invokes [applyAttributes](#applyAttributes)
+ * * `childNodes`: invokes [applyChildNodes](#applyChildNodes)
+ * * `classes`: invokes [applyClasses](#applyClasses)
+ * * `style`: invokes [applyStyles](#applyStyles)
+ * * `$`: applies updates to referenced elements in the component's shadow tree.
+ *   (See below.)
+ * 
+ * All other `name: value` entries are interpreted as custom properties.
+ * 
+ * Example:
+ * 
+ *     import * as updates from 'elix/src/updates.js';
+ * 
+ *     const div = document.createElement('div');
+ *     div.classList.add('foo bar');
+ *     updates.apply({
+ *       classList: {
+ *         bar: false, // Removes bar
+ *         baz: true   // Adds baz
+ *       }
+ *     });
+ *     div.classList.value // "foo baz"
+ * 
+ * Note that `apply` _updates_ the indicated properties, leaving in place any
+ * other existing element attributes, classes, or styles not specifically
+ * referenced in the dictionary (e.g., the class "foo" above).
+ * 
+ * As noted above, `apply` can be used to set custom element properties. E.g.,
+ * 
+ *     updates.apply(element, { foo: 'bar' });
+ * 
+ * is equivalent to calling:
+ * 
+ *     element.foo = 'bar';
+ * 
+ * @param {Element} element - the element to update
+ * @param {object} updates - the updates to apply
  */
-export function apply(element, props) {
+export function apply(element, updates) {
   const applyFunctionForProperty = {
-    '$': applyReferencedElementProps,
+    '$': applyReferencedElementUpdates,
     'attributes': applyAttributes,
     'childNodes': applyChildNodes,
     'classes': applyClasses,
-    'style': applyStyle
+    'style': applyStyles
   };
-  for (const key in props) {
-    const value = props[key];
+  for (const key in updates) {
+    const value = updates[key];
     const applyFunction = applyFunctionForProperty[key];
     if (applyFunction) {
       applyFunction(element, value);
@@ -56,9 +110,22 @@ export function apply(element, props) {
 
 
 /**
- * @param {Element} element 
- * @param {string} name 
- * @param {string|boolean} value 
+ * Update the given attribute on an element.
+ * 
+ * Passing a non-null `value` acts like a call to `setAttribute(name, value)`.
+ * If the supplied `value` is nullish, this acts like a call to
+ * `removeAttribute(name)`.
+ * 
+ * Boolean attributes are treated specially. If the attribute `name` matches one
+ * of the known HTML boolean attributes (`checked`, `defer`, `disabled`,
+ * `hidden`, `ismap`, `multiple`, `noresize`, `readonly`, `selected`), the
+ * attribute will be handled as a boolean attribute. A truthy `value` acts like
+ * a call to `setAttribute(name, '')`. A falsy value acts like
+ * `removeAttribute(name)`.
+ * 
+ * @param {Element} element - the element to update
+ * @param {string} name - the attribute name
+ * @param {string|boolean} value - the attribute value to set
  */
 export function applyAttribute(element, name, value) {
   if (booleanAttributes[name]) {
@@ -80,21 +147,36 @@ export function applyAttribute(element, name, value) {
 
 
 /**
- * @param {Element} element 
- * @param {any} attributeProps
+ * Apply the indicated attributes to the element.
+ * 
+ * This takes a dictionary in which each `name: value` is results in a call to
+ * `applyAttribute(name, value)`. See [applyAttribute](#applyAttribute) for
+ * details.
+ * 
+ * @param {Element} element - the element to update
+ * @param {object} attributes - a dictionary of attributes to apply
  */
-export function applyAttributes(element, attributeProps) {
-  if (attributeProps) {
-    for (const attributeName in attributeProps) {
-      applyAttribute(element, attributeName, attributeProps[attributeName]);
+export function applyAttributes(element, attributes) {
+  if (attributes) {
+    for (const attributeName in attributes) {
+      applyAttribute(element, attributeName, attributes[attributeName]);
     }
   }
 }
 
 
 /**
- * @param {Element} element 
- * @param {NodeList|Node[]} childNodes
+ * Sets the element's `childNodes` to the given set of nodes.
+ * 
+ * This adds or removes the element's `childNodes` as necessary to match the
+ * nodes indicated in the `childNodes` parameter.
+ * 
+ * As a performance optimization, if the supplied `childNodes` value is frozen
+ * and equivalent to the last call made to this function, the function assumes
+ * the child nodes are already correct and does no work.
+ * 
+ * @param {Element} element - the element to update
+ * @param {NodeList|Node[]} childNodes - the set of nodes to apply
  */
 export function applyChildNodes(element, childNodes) {
   // Quick dirty check if last array applied was frozen.
@@ -124,20 +206,23 @@ export function applyChildNodes(element, childNodes) {
 
 
 /**
- * @param {Element} element 
- * @param {any} classProps
+ * Applies multiple classes at once. This takes a dictionary in which each
+ * `name: value` is equivalent to calling `classList.toggle(name, value)`.
+ * 
+ * @param {Element} element - the element to update
+ * @param {object} classes - a dictionary of classes to apply
  */
-export function applyClasses(element, classProps) {
-  for (const className in classProps) {
-    const value = classProps[className] || false;
+export function applyClasses(element, classes) {
+  for (const className in classes) {
+    const value = classes[className] || false;
     element.classList.toggle(className, value);
   }
 }
 
 
-function applyReferencedElementProps(element, referencedElementProps) {
-  for (const key in referencedElementProps) {
-    const props = referencedElementProps[key];
+function applyReferencedElementUpdates(element, updates) {
+  for (const key in updates) {
+    const props = updates[key];
     const referencedElement = element.$[key];
     apply(referencedElement, props);
   }
@@ -145,28 +230,38 @@ function applyReferencedElementProps(element, referencedElementProps) {
 
 
 /**
- * @param {HTMLElement|SVGElement} element 
- * @param {any} styleProps
+ * Sets multiple style values at once.
+ * 
+ * This takes a dictionary in which each `name: value` is equivalent to calling
+ * `style[name] = value`.
+ * 
+ * @param {HTMLElement|SVGElement} element - the element to update
+ * @param {object} styles - a dictionary of styles to apply
  */
-export function applyStyle(element, styleProps) {
-  const style = element.style;
-  for (const styleName in styleProps) {
-    const value = styleProps[styleName];
-    style[styleName] = value === undefined ? '' : value;
+export function applyStyles(element, styles) {
+  for (const styleName in styles) {
+    const value = styles[styleName];
+    element.style[styleName] = value === undefined ? '' : value;
   }
 }
 
 
 /**
- * @param {Element} element
- * @returns {any}
+ * Returns a dictionary of the current attributes, classes, and styles of the
+ * indicated element.
+ * 
+ * The returned dictionary is in the same format as that supported by
+ * [apply](#apply).
+ * 
+ * @param {Element} element - the element to examine
+ * @returns {object} a dictionary of the current attributes, classes, and styles
  */
 export function current(element) {
   return element instanceof HTMLElement ?
     {
       attributes: currentAttributes(element),
       classes: currentClasses(element),
-      style: currentStyle(element)
+      style: currentStyles(element)
     } :
     {
       attributes: currentAttributes(element),
@@ -176,8 +271,11 @@ export function current(element) {
 
 
 /**
- * @param {Element} element
- * @returns {any}
+ * Returns a dictionary of the element's current attributes, using the same
+ * `name: value` format supported by [applyAttributes](#applyAttributes).
+ * 
+ * @param {Element} element - the element to examine
+ * @returns {object} a dictionary of the element's current attributes
  */
 export function currentAttributes(element) {
   const attributes = {};
@@ -192,8 +290,11 @@ export function currentAttributes(element) {
 
 
 /**
- * @param {Element} element
- * @returns {any}
+ * Returns a dictionary of the element's current classes, using the same
+ * `name: value` format supported by [applyClasses](#applyClasses).
+ * 
+ * @param {Element} element - the element to examine
+ * @returns {object} a dictionary of the element's current classes
  */
 export function currentClasses(element) {
   const result = {};
@@ -205,10 +306,13 @@ export function currentClasses(element) {
 
 
 /**
- * @param {HTMLElement|SVGElement} element
- * @returns {any}
+ * Returns a dictionary of the element's current styles, using the same
+ * `name: value` format supported by [applyStyles](#applyStyles).
+ *
+ * @param {HTMLElement|SVGElement} element - the element to update
+ * @returns {object} a dictionary of the element's current styles
  */
-export function currentStyle(element) {
+export function currentStyles(element) {
   const styleProps = {};
   Array.prototype.forEach.call(element.style, key => {
     styleProps[key] = element.style[key];
@@ -218,8 +322,32 @@ export function currentStyle(element) {
 
 
 /**
- * @param {any[]} sources
- * @returns {any}
+ * Merge multiple sets of updates into a single update dictionary.
+ * 
+ * This is similar to `Object.assign`, but:
+ * * A new dictionary is always returned instead of merging into the first
+ *   argument.
+ * * The special keys `attributes`, `childNodes`, `classes`, `style`, `$`
+ *   are handled with a deep merge instead of a shallow merge.
+ * 
+ * Example:
+ * 
+ *     merge({
+ *       attributes: { role: 'listbox' },
+ *       classes: { foo: true, bar: true }
+ *     }, {
+ *       classes: { foo: false, baz: true }
+ *     });
+ * 
+ * returns
+ * 
+ *     {
+ *       attributes: { role: 'listbox' },
+ *       classes: { foo: false, bar: true, baz: true }
+ *     }
+ * 
+ * @param {...object} sources - a set of zero or more update dictionaries to merge
+ * @returns {object} the merged updates
  */
 export function merge(...sources) {
   const result = {};
