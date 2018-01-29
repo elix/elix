2152178c5a24607c558ec850e66ec6ae6d259f03
Jan Miksovsky <jan@quickui.org>
2018-01-29 14:06:54 -0800

    Try define elix.js as the main package entry point. Include all mixins/helpers in elix.js.

---
+++ src/elix.js
@@ -0,0 +1,83 @@
+/*
+ * The complete set of Elix elements and mixins.
+ * 
+ * This file is the primary entry point to the Elix package, so its exports are
+ * what is obtained if you write `import * from 'elix'`. However, you can opt
+ * to ignore this file, and directly import the sources you want from the /src
+ * folder.
+ * 
+ * This file is also used during testing, as it causes all Elix's elements to be
+ * loaded/built.
+ */
+
+
+// Files that export a single object.
+export { default as AlertDialog } from './AlertDialog.js';
+export { default as AriaListMixin } from './AriaListMixin.js';
+export { default as ArrowDirectionButton } from './ArrowDirectionButton.js';
+export { default as ArrowDirectionMixin } from './ArrowDirectionMixin.js';
+export { default as AttributeMarshallingMixin } from './AttributeMarshallingMixin.js';
+export { default as AutosizeTextarea } from './AutosizeTextarea.js';
+export { default as ClickSelectionMixin } from './ClickSelectionMixin.js';
+export { default as ContentItemsMixin } from './ContentItemsMixin.js';
+export { default as Dialog } from './Dialog.js';
+export { default as DialogModalityMixin } from './DialogModalityMixin.js';
+export { default as DirectionSelectionMixin } from './DirectionSelectionMixin.js';
+export { default as Drawer } from './Drawer.js';
+export { default as ElementBase } from './ElementBase.js';
+export { default as ExpandablePanel } from './ExpandablePanel.js';
+export { default as FocusCaptureMixin } from './FocusCaptureMixin.js';
+export { default as FocusVisibleMixin } from './FocusVisibleMixin.js';
+export { default as HoverMixin } from './HoverMixin.js';
+export { default as KeyboardDirectionMixin } from './KeyboardDirectionMixin.js';
+export { default as KeyboardMixin } from './KeyboardMixin.js';
+export { default as KeyboardPagedSelectionMixin } from './KeyboardPagedSelectionMixin.js';
+export { default as KeyboardPrefixSelectionMixin } from './KeyboardPrefixSelectionMixin.js';
+export { default as LanguageDirectionMixin } from './LanguageDirectionMixin.js';
+export { default as ListBox } from './ListBox.js';
+export { default as ModalBackdrop } from './ModalBackdrop.js';
+export { default as Modes } from './Modes.js';
+export { default as OpenCloseMixin } from './OpenCloseMixin.js';
+export { default as OverlayMixin } from './OverlayMixin.js';
+export { default as PageDot } from './PageDot.js';
+export { default as PageDotsMixin } from './PageDotsMixin.js';
+export { default as Popup } from './Popup.js';
+export { default as PopupModalityMixin } from './PopupModalityMixin.js';
+export { default as ReactiveMixin } from './ReactiveMixin.js';
+export { default as RenderUpdatesMixin } from './RenderUpdatesMixin.js';
+export { default as SelectedItemTextValueMixin } from './SelectedItemTextValueMixin.js';
+export { default as SelectionInViewMixin } from './SelectionInViewMixin.js';
+export { default as ShadowTemplateMixin } from './ShadowTemplateMixin.js';
+export { default as SingleSelectionMixin } from './SingleSelectionMixin.js';
+export { default as SlidingCarousel } from './SlidingCarousel.js';
+export { default as SlidingPages } from './SlidingPages.js';
+export { default as SlidingViewport } from './SlidingViewport.js';
+export { default as SlotContentMixin } from './SlotContentMixin.js';
+export { default as SwipeDirectionMixin } from './SwipeDirectionMixin.js';
+export { default as Symbol } from './Symbol.js';
+export { default as TabButton } from './TabButton.js';
+export { default as TabStrip } from './TabStrip.js';
+export { default as Tabs } from './Tabs.js';
+export { default as Toast } from './Toast.js';
+export { default as TouchSwipeMixin } from './TouchSwipeMixin.js';
+export { default as TrackpadSwipeMixin } from './TrackpadSwipeMixin.js';
+export { default as TransitionEffectMixin } from './TransitionEffectMixin.js';
+export { default as WrappedStandardElement } from './WrappedStandardElement.js';
+export { default as constants } from './constants.js';
+export { default as deepContains } from './deepContains.js';
+export { default as defaultScrollTarget } from './defaultScrollTarget.js';
+
+
+// Files that export multiple objects.
+// As of Jan 2018, there's no way to simultaneously import a collection of
+// objects and then export them as a named object, so we have to do the
+// import and export in separate steps.
+import * as contentImport from './content.js';
+import * as fractionalSelectionImport from './fractionalSelection.js';
+import * as symbolsImport from './symbols.js';
+import * as updatesImport from './updates.js';
+
+export const content = contentImport;
+export const fractionalSelection = fractionalSelectionImport;
+export const symbols = symbolsImport;
+export const updates = updatesImport;
