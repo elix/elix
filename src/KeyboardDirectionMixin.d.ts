b2a0e406d0f45cfaff3a94b64a959c3a854d74c2
Jan Miksovsky <jan@quickui.org>
2018-01-31 10:51:34 -0800

    Simplify path.

--- src/KeyboardDirectionMixin.d.ts
+++ src/KeyboardDirectionMixin.d.ts
@@ -1,7 +1,7 @@
 // Elix is a JavaScript project, but we use TypeScript as an internal tool to
 // confirm our code is type safe.
 
-/// <reference path="../src/shared.d.ts"/>
+/// <reference path="shared.d.ts"/>
 
 declare const KeyboardDirectionMixin: StateMixin<{}, {}, {}, {
   orientation: string;
--- src/Symbol.d.ts
+++ src/Symbol.d.ts
@@ -1,7 +1,7 @@
 // Elix is a JavaScript project, but we use TypeScript as an internal tool to
 // confirm our code is type safe.
 
-/// <reference path="../src/shared.d.ts"/>
+/// <reference path="shared.d.ts"/>
 
 // Our polyfill will return a string on IE 11, but for type-checking purposes,
 // we can assume we'll always get a real symbol.
