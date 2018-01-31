ec3e2fadb94f22de3c07a1db35668e6549d904de
Jan Miksovsky <jan@quickui.org>
2018-01-31 09:12:30 -0800

    Simplify definition of SlidingCarousel.

--- demos/src/CustomCarousel1.js
+++ demos/src/CustomCarousel1.js
@@ -1,6 +1,4 @@
-import './CustomArrowButton.js';
 import * as symbols from '../../src/symbols.js';
-import { merge } from '../../src/updates.js';
 import ArrowDirectionMixin from '../../src/ArrowDirectionMixin.js';
 import PageDotsMixin from '../../src/PageDotsMixin.js';
 import SlidingPages from '../../src/SlidingPages.js';
--- src/SlidingCarousel.js
+++ src/SlidingCarousel.js
@@ -1,40 +1,15 @@
-import './SlidingViewport.js';
-import { merge } from './updates.js';
 import * as symbols from './symbols.js';
-import AriaListMixin from './AriaListMixin.js';
 import ArrowDirectionMixin from './ArrowDirectionMixin.js';
-import ContentItemsMixin from './ContentItemsMixin.js';
-import DirectionSelectionMixin from './DirectionSelectionMixin.js';
-import ElementBase from './ElementBase.js';
-import FocusVisibleMixin from './FocusVisibleMixin.js';
-import KeyboardDirectionMixin from './KeyboardDirectionMixin.js';
-import KeyboardMixin from './KeyboardMixin.js';
-import LanguageDirectionMixin from './LanguageDirectionMixin.js';
 import PageDotsMixin from './PageDotsMixin.js';
-import SingleSelectionMixin from './SingleSelectionMixin.js';
-import SlotContentMixin from './SlotContentMixin.js';
-import SwipeDirectionMixin from './SwipeDirectionMixin.js';
-import TouchSwipeMixin from './TouchSwipeMixin.js';
-import TrackpadSwipeMixin from './TrackpadSwipeMixin.js';
+import SlidingPages from './SlidingPages.js';
 
 
 const Base =
-  AriaListMixin(
   ArrowDirectionMixin(
-  ContentItemsMixin(
-  DirectionSelectionMixin(
-  FocusVisibleMixin(
-  KeyboardDirectionMixin(
-  KeyboardMixin(
-  LanguageDirectionMixin(
   PageDotsMixin(
-  SingleSelectionMixin(
-  SlotContentMixin(
-  SwipeDirectionMixin(
-  TouchSwipeMixin(
-  TrackpadSwipeMixin(
-    ElementBase
-  ))))))))))))));
+    SlidingPages
+  ));
+
 
 /**
  * A typical carousel with a sliding effect. The user can move between items with
@@ -45,72 +20,20 @@ const Base =
  * at the bottom of the carousel provided by [PageDotsMixin](PageDotsMixin). For
  * a plain carousel without those extras, see [SlidingPages](SlidingPages).
  * 
- * @inherits ElementBase
- * @mixes AriaListMixin
+ * @inherits SlidingPages
  * @mixes ArrowDirectionMixin
- * @mixes ContentItemsMixin
- * @mixes DirectionSelectionMixin
- * @mixes FocusVisibleMixin
- * @mixes KeyboardDirectionMixin
- * @mixes KeyboardMixin
- * @mixes LanguageDirectionMixin
  * @mixes PageDotsMixin
- * @mixes SingleSelectionMixin
- * @mixes SlotContentMixin
- * @mixes SwipeDirectionMixin
- * @mixes TouchSwipeMixin
- * @mixes TrackpadSwipeMixin
  */
 class SlidingCarousel extends Base {
 
-  componentDidMount() {
-    if (super.componentDidMount) { super.componentDidMount(); }
-    this.$.viewport.addEventListener('selected-index-changed', () => {
-      /** @type {any} */
-      const viewport = this.$.viewport;
-      this.selectedIndex = viewport.selectedIndex;
-    });
-  }
-
-  get defaultState() {
-    return Object.assign({}, super.defaultState, {
-      selectionRequired: true
-    });
-  }
-
   get [symbols.template]() {
-    return `
-      <style>
-        :host {
-          display: flex;
-          overflow: hidden;
-          position: relative;
-        }
-
-        elix-sliding-viewport {
-          flex: 1;
-        }
-      </style>
-      ${this[ArrowDirectionMixin.inject](
-        this[PageDotsMixin.inject](`
-          <elix-sliding-viewport id="viewport">
-            <slot></slot>
-          </elix-sliding-viewport>
-        `)
-      )}
-    `;
+    return this[ArrowDirectionMixin.inject](
+      this[PageDotsMixin.inject](
+        super[symbols.template]
+      )
+    );
   }
 
-  get updates() {
-    return merge(super.updates, {
-      $: {
-        viewport: {
-          selectedIndex: this.state.selectedIndex,
-          swipeFraction: this.state.swipeFraction
-        }
-      }
-    });
-  }
 }
 
 
