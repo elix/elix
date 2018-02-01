c56be634e60fc2b9cd50599f2cd97a841be69be5
Jan Miksovsky <jan@quickui.org>
2018-01-31 16:46:35 -0800

    Customize all the things.

---
+++ demos/src/CustomCarousel5.js
@@ -0,0 +1,63 @@
+import './CustomArrowButton.js';
+import './CustomPageDot.js';
+import { merge } from '../../src/updates.js';
+import * as symbols from '../../src/symbols.js';
+import ArrowDirectionMixin from '../../src/ArrowDirectionMixin.js';
+import PageDotsMixin from '../../src/PageDotsMixin.js';
+import PageNumbersMixin from './PageNumbersMixin.js';
+import SlidingPages from '../../src/SlidingPages.js';
+
+
+const Base =
+  ArrowDirectionMixin(
+  PageDotsMixin(
+  PageNumbersMixin(
+    SlidingPages
+  )));
+
+
+// Customize everything.
+class CustomCarousel extends Base {
+
+  get tags() {
+    return Object.assign({}, super.tags, {
+      arrowButton: 'custom-arrow-button',
+      pageDot: 'custom-page-dot'
+    });
+  }
+
+  get [symbols.template]() {
+    return this[ArrowDirectionMixin.inject](
+      this[PageDotsMixin.inject](
+        this[PageNumbersMixin.inject](
+          super[symbols.template]
+        )
+      )
+    );
+  }
+
+  get updates() {
+    const arrowButtonStyle = {
+      'font-size': '28px',
+      'font-weight': 'bold',
+      padding: '0.5em'
+    };
+    return merge(super.updates, {
+      $: {
+        arrowButtonLeft: {
+          style: arrowButtonStyle,
+          textContent: "↫"
+        },
+        arrowButtonRight: {
+          style: arrowButtonStyle,
+          textContent: "↬"
+        }
+      }
+    });
+  }
+
+}
+
+
+customElements.define('custom-carousel', CustomCarousel);
+export default CustomCarousel;
