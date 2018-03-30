d84908424bfa24f39b19a24ead2afa2f63762579
Jan Miksovsky <jan@quickui.org>
2018-03-30 16:56:36 -0700

    Clean out dead code.

--- src/Popup.js
+++ src/Popup.js
@@ -1,5 +1,3 @@
-import './OverlayFrame.js';
-import * as symbols from './symbols.js';
 import Overlay from './Overlay.js';
 import KeyboardMixin from './KeyboardMixin.js';
 import PopupModalityMixin from './PopupModalityMixin.js';
@@ -20,38 +18,7 @@ const Base =
  * @mixes KeyboardMixin
  * @mixes PopupModalityMixin
  */
-class Popup extends Base {
-
-  // get [symbols.template]() {
-  //   const frameTag = this.frameTag || this.defaults.tags.frame;
-  //   return `
-  //     <style>
-  //       :host {
-  //         align-items: center;
-  //         display: flex;
-  //         flex-direction: column;
-  //         height: 100%;
-  //         justify-content: center;
-  //         left: 0;
-  //         outline: none;
-  //         pointer-events: none;
-  //         position: fixed;
-  //         top: 0;
-  //         -webkit-tap-highlight-color: transparent;
-  //         width: 100%;
-  //       }
-
-  //       #frame {
-  //         pointer-events: initial;
-  //       }
-  //     </style>
-  //     <${frameTag} id="frame">
-  //       <slot></slot>
-  //     </${frameTag}>
-  //   `;
-  // }
-
-}
+class Popup extends Base {}
 
 
 customElements.define('elix-popup', Popup);
