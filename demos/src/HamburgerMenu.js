8889b2754ac6fff7ff4610f8f589b32237f16d91
Jan Miksovsky <jan@miksovsky.com>
2018-05-08 17:10:26 -0700

    Clean up hamburger SVG to remove extra margin. Styling.

--- demos/src/HamburgerMenu.js
+++ demos/src/HamburgerMenu.js
@@ -1,6 +1,6 @@
+import '../../src/Drawer.js';
 import './QuietButton.js';
 import { merge } from '../../src/updates.js';
-import Drawer from '../../src/Drawer.js';
 import OpenCloseMixin from '../../src/OpenCloseMixin.js';
 import ReactiveElement from '../../src/ReactiveElement.js';
 import * as symbols from '../../src/symbols.js';
@@ -12,6 +12,10 @@ const Base =
   );
 
 
+/**
+ * A button that invokes a Drawer, typically used to provide navigation and
+ * other UI on a mobile device.
+ */
 export default class HamburgerMenu extends Base {
 
   componentDidMount() {
@@ -21,24 +25,35 @@ export default class HamburgerMenu extends Base {
       this[symbols.raiseChangeEvents] = false;
     });
     this.$.drawer.addEventListener('opened-changed', event => {
+      /** @type {any} */
+      const cast = event;
       this.setState({
-        opened: event.detail.opened
+        opened: cast.detail.opened
       });
     });
   }
 
   get [symbols.template]() {
-    // Hamburger icon from Google's Material Icons collection
     return `
       <style>
         :host {
           display: inline-block;
         }
+
+        #menuButton {
+          display: block;
+          height: 1.5em;
+          width: 1.5em;
+        }
+
+        #hamburgerIcon {
+          height: 100%;
+          width: 100%;
+        }
       </style>
       <quiet-button id="menuButton" aria-label="Open menu">
-        <svg fill="#000000" height="18" viewBox="0 0 18 18" width="18" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+        <svg id="hamburgerIcon" viewBox="0 0 18 12" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0 h18 v2 h-18 z m0 5 h18 v2 h-18 z m0 5 h18 v2 h-18 z"></path>
         </svg>
       </quiet-button>
       <elix-drawer id="drawer">
@@ -48,11 +63,10 @@ export default class HamburgerMenu extends Base {
   }
 
   get updates() {
-    const opened = this.opened;
     return merge(super.updates, {
       $: {
         drawer: {
-          opened
+          opened: this.opened
         }
       }
     });
