c9ca91df392f29cd8ded56e81ccfb7916e8bfd1f
Jan Miksovsky <jan@miksovsky.com>
2018-05-15 14:10:20 -0700

    Popup positioning

--- src/PopupSource.js
+++ src/PopupSource.js
@@ -29,12 +29,31 @@ class PopupSource extends Base {
       }
     });
     this.$.popup.addEventListener('opened-changed', event => {
-      const opened = event.detail.opened;
+      /** @type {any} */ 
+      const cast = event;
+      const opened = cast.detail.opened;
       if (opened !== this.opened) {
         this[symbols.raiseChangeEvents] = true;
         this.opened = opened;
         this[symbols.raiseChangeEvents] = false;
       }
+    });
+  }
+
+  get defaultState() {
+    return Object.assign({}, super.defaultState, {
+      horizontalAlign: 'left',
+      preferredPosition: 'below'
+    });
+  }
+
+  // TODO: 'start', 'end'
+  get horizontalAlign() {
+    return this.state.horizontalAlign;
+  }
+  set horizontalAlign(horizontalAlign) {
+    this.setState({
+      horizontalAlign
     });
   }
 
@@ -53,24 +72,106 @@ class PopupSource extends Base {
     return handled || (super[symbols.keydown] && super[symbols.keydown](event));
   }
 
+  get preferredPosition() {
+    return this.state.preferredPosition;
+  }
+  set preferredPosition(preferredPosition) {
+    this.setState({
+      preferredPosition
+    });
+  }
+
   // TODO: Tags for popup and source
   get [symbols.template]() {
     return `
+      <style>
+        :host {
+          display: inline-block;
+          position: relative;
+        }
+
+        #button {
+          background-color: transparent;
+          border-style: solid;
+          display: block;
+        }
+
+        #popupContainer {
+          height: 0;
+          position: absolute;
+          width: 100%;
+        }
+
+        #popup {
+          align-items: initial;
+          flex-direction: initial;
+          height: initial;
+          justify-content: initial;
+          left: initial;
+          position: absolute;
+          top: initial;
+          width: initial;
+        }
+      </style>
       <button id="button" tabindex="-1">
         <slot></slot>
       </button>
-      <elix-popup id="popup">
-        <slot name="popup"></slot>
-      </elix-popup>
+      <div id="popupContainer">
+        <elix-popup id="popup">
+          <slot name="popup"></slot>
+        </elix-popup>
+      </div>
     `;
   }
 
   // TODO: Pressed state for button
   get updates() {
+
+    const buttonStyle = {
+      'background-color': this.state.opened ? 'highlight' : ''
+    };
+
+    const positionBelow = this.state.preferredPosition === 'below';
+
+    const popupContainerStyle = positionBelow ?
+      {
+        bottom: '',
+        position: 'relative',
+        top: ''
+      } :
+      {
+        bottom: 0,
+        position: 'absolute',
+        top: 0
+      };
+    
+    const popupStyle = positionBelow ?
+      {
+        bottom: ''
+      } :
+      {
+        bottom: 0
+      };
+    
+    const horizontalAlign = this.state.horizontalAlign;
+    popupStyle.left = horizontalAlign === 'left' || horizontalAlign === 'stretch' ?
+      '0' :
+      '';
+    popupStyle.right = horizontalAlign === 'right' || horizontalAlign === 'stretch' ?
+      '0' :
+      '';
+
     return merge(super.updates, {
       $: {
+        button: {
+          style: buttonStyle
+        },
         popup: {
-          opened: this.state.opened
+          opened: this.state.opened,
+          style: popupStyle
+        },
+        popupContainer: {
+          style: popupContainerStyle
         }
       }
     });
