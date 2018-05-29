11750441e63aabf4c8a3c1f369d0c182509d64fb
Jan Miksovsky <jan@miksovsky.com>
2018-05-29 14:56:15 -0700

    Use opacity: 0 (instead of visibility: hidden) to hide popup until it's ready.

--- src/PopupSource.js
+++ src/PopupSource.js
@@ -302,14 +302,17 @@ class PopupSource extends Base {
     }
 
     // Until we've checked the rendered position of the popup, keep it in the
-    // layout but don't make it visible yet.
-    const visibility = fitChecked ? '' : 'hidden';
+    // layout but don't make it visible yet. If we use `visibility: hidden` for
+    // this purpose, the popup won't be able to receive the focus. Instead, we
+    // use zero opacity as a way to make the popup temporarily invisible until
+    // we have checked where it fits.
+    const opacity = fitChecked ? '' : 0;
 
     const popupStyle = {
       bottom,
       left,
-      right,
-      visibility
+      opacity,
+      right
     };
 
     return merge(base, {
