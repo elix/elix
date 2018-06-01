01df8be85514a394ad53af469e86558e4643d3e6
Jan Miksovsky <jan@miksovsky.com>
2018-05-31 18:08:50 -0700

    Styling

--- src/MenuButton.js
+++ src/MenuButton.js
@@ -177,7 +177,7 @@ class MenuButton extends Base {
   get sourceSlotContent() {
     // Default "..." icon from Google Material Design icons.
     return `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <svg id="ellipsisIcon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
         <path d="M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
       </svg>
     `;
@@ -203,6 +203,15 @@ class MenuButton extends Base {
           style: {
             'align-items': 'center',
             display: 'flex'
+          }
+        }
+      }
+    },
+    this.$.ellipsisIcon && {
+      $: {
+        ellipsisIcon: {
+          style: {
+            fill: 'currentColor'
           }
         }
       }
