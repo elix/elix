eec3d48f832f68efa5b5bdc4ef996b05e2f7c10c
Jan Miksovsky <jan@miksovsky.com>
2018-08-22 14:48:51 -0700

    Type declaration workaround for symbols.js.

---
+++ src/symbols.d.ts
@@ -0,0 +1,43 @@
+/*
+ * The Elix project makes pervasive use of shared Symbol objects as indexers
+ * to avoid accidental name conflicts.
+ * 
+ * Unforunately, as of Aug 2018, TypeScript no longer considers this to be
+ * valid, and will log a type error. See
+ * https://github.com/Microsoft/TypeScript/issues/1863.
+ * 
+ * The best workaround we have found is to declare shared Symbol objects (below,
+ * and also inline in various components and mixins) to be string literals
+ * instead. This suppresses the type errors, while still allowing the real
+ * Symbol objects to be used at runtime as desired.
+ */
+
+export const canGoLeft: '_canGoLeft';
+export const canGoRight: '_canGoRight';
+export const click: '_click';
+export const contentSlot: '_contentSlot';
+export const defaultFocus: '_defaultFocus';
+export const descriptors: '_descriptors';
+export const elementsWithTransitions: '_elementsWithTransitions';
+export const getItemText: '_getItemText';
+export const goDown: '_goDown';
+export const goEnd: '_goEnd';
+export const goLeft: '_goLeft';
+export const goRight: '_goRight';
+export const goStart: '_goStart';
+export const goUp: '_goUp';
+export const hasDynamicTemplate: '_hasDynamicTemplate';
+export const keydown: '_keydown';
+export const mouseenter: '_mouseenter';
+export const mouseleave: '_mouseleave';
+export const event: '_event';
+export const raiseChangeEvents: '_raiseChangeEvents';
+export const render: '_render';
+export const rendering: '_rendering';
+export const rightToLeft: '_rightToLeft';
+export const scrollTarget: '_scrollTarget';
+export const startEffect: '_startEffect';
+export const swipeLeft: '_swipeLeft';
+export const swipeRight: '_swipeRight';
+export const swipeTarget: '_swipeTarget';
+export const template: '_template';
