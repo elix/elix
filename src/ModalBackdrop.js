ae58c5c12b1a0a5d0882ced653e35f7df4da003b
Jan Miksovsky <jan@miksovsky.com>
2018-09-06 17:24:36 -0700

    Try to prevent drags on a modal backdrop from scrolling the page in the background.

--- src/ModalBackdrop.js
+++ src/ModalBackdrop.js
@@ -11,6 +11,26 @@ import Backdrop from './Backdrop.js';
  */
 class ModalBackdrop extends Backdrop {
 
+  constructor() {
+    super();
+
+    // As of Sep 2018, Mobile Safari allows the user to drag on the backdrop to
+    // scroll the page behind it, which violates the modality. To correct this,
+    // we prevent touchmove events with one touch from performing the default
+    // page scrolling.
+    //
+    // Android Chrome already correctly prevents drags from reaching the page,
+    // so we only engage this workaround if we don't see support for pointer
+    // events -- in which case we assume we're in Safari.
+    if (!('PointerEvent' in window)) {
+      this.addEventListener('touchmove', event => {
+        if (event.touches.length === 1) {
+          event.preventDefault();
+        }
+      });
+    }
+  }
+
   get updates() {
     return merge(super.updates, {
       style: {
