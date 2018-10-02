07f576914728a281019ce1c0cbdd8152f320f61c
Jan Miksovsky <jan@miksovsky.com>
2018-10-02 10:32:57 -0700

    Try using Express to server static files for headless and Sauce Labs testing. Not working yet.

--- tasks/headless-tests.js
+++ tasks/headless-tests.js
@@ -1,15 +1,19 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
+const express = require('express');
 const getPort = require('get-port');
-const StaticServer = require('static-server');
+const path = require('path');
+const puppeteer = require('puppeteer');
 const runInCi = process.argv.indexOf('--run-in-ci');
+const StaticServer = require('static-server');
 
-const startStaticHttpServer = async () => {
-  const server = new StaticServer({
-    rootPath: path.join(__dirname, '..'),
-    port: await getPort()
+
+const startStaticHttpServer = async (port) => {
+  const app = express();
+  const rootPath = path.join(__dirname, '..');
+  app.use('/', express.static(rootPath));
+  let server;
+  await new Promise(resolve => {
+    server = app.listen(port, resolve);
   });
-  await new Promise((resolve) => {server.start(resolve);});
   return server;
 };
 
@@ -30,14 +34,20 @@ const runTestsInHeadlessChrome = async (port) => {
 };
 
 (async () => {
-  const server = await startStaticHttpServer();
-  const testResult = await runTestsInHeadlessChrome(server.port);
-  if (testResult === 'OK') {
-    console.log('Tests passed.');
-  } else {
-    const errors = JSON.parse(testResult);
-    errors.map(e => console.log(e));
-    console.error(`\n${errors.length} Error(s)`);
+  try {
+    const port = await getPort();
+    const server = await startStaticHttpServer(port);
+    const testResult = await runTestsInHeadlessChrome(port);
+    if (testResult === 'OK') {
+      console.log('Tests passed.');
+    } else {
+      const errors = JSON.parse(testResult);
+      errors.map(e => console.log(e));
+      console.error(`\n${errors.length} Error(s)`);
+    }
+    server.close();
+  } catch (exception) {
+    // We have to handle the exception, since nothing else will.
+    console.error(`*** EXCEPTION: ${exception}`);
   }
-  server.stop();
 })();
