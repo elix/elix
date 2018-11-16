464a4661a6aef80f3ac96bbcc067bda1939901b1
Jan Miksovsky <jan@miksovsky.com>
2018-11-16 13:54:13 -0800

    Initial highlight of matching text.

--- src/FilterListBox.js
+++ src/FilterListBox.js
@@ -50,22 +50,56 @@ class FilterListBox extends Base {
     });
   }
 
+  highlightTextInNode(textToHighlight, node) {
+    const text = node.textContent;
+    const start = text.toLowerCase().indexOf(textToHighlight);
+    if (textToHighlight && start >= 0) {
+      const end = start + textToHighlight.length;
+      const part1 = text.substr(0, start);
+      const part2 = text.substring(start, end);
+      const part3 = text.substr(end);
+      const fragment = document.createDocumentFragment();
+      fragment.appendChild(new Text(part1));
+      const strong = document.createElement('strong');
+      strong.textContent = part2;
+      fragment.appendChild(strong);
+      fragment.appendChild(new Text(part3));
+      return fragment.childNodes;
+    } else {
+      return [new Text(text)];
+    }
+  }
+
   itemUpdates(item, calcs, original) {
     const base = super.itemUpdates ? super.itemUpdates(item, calcs, original) : {};
     const { matches, selected } = calcs;
     const color = selected ? 'highlighttext' : original.style.color;
     const backgroundColor = selected ? 'highlight' : original.style['background-color'];
     const display = matches ? original.style.display : 'none';
-    return merge(base, {
-      classes: {
-        selected
+    // TODO: Generalize
+    const singleTextNode = item.childNodes.length === 1 &&
+      item.childNodes[0] instanceof Text;
+    const textToHighlight = matches ?
+      this.state.filter.toLowerCase() :
+      null;
+    const childNodes = singleTextNode && !matches ?
+      null :
+      this.highlightTextInNode(textToHighlight, item);
+    return merge(base, Object.assign(
+      {
+        classes: {
+          selected
+        },
+        style: {
+          'background-color': backgroundColor,
+          color,
+          display
+        }
       },
-      style: {
-        'background-color': backgroundColor,
-        color,
-        display
-      }
-    });
+      childNodes && {
+        childNodes
+      })
+    );
   }
 
   get orientation() {
