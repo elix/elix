b7d658c832f37dc42729bd61a6478fab9ca1d969
Jan Miksovsky <jan@miksovsky.com>
2019-07-31 12:04:43 -0700

    getScrollableElement uses new composedAncestors utility.

--- src/scrolling.js
+++ src/scrolling.js
@@ -1,3 +1,6 @@
+import { composedAncestors } from "./utilities.js";
+
+
 /**
  * Utilities for working with scrolling.
  * 
@@ -29,27 +32,71 @@ export function defaultScrollTarget(element) {
 
 
 /**
- * If the given element can be scrolled, return that. If not, return the closest
- * ancestor that can be scrolled. If no such ancestor is found, return null.
+ * Return true if the given element can be scrolled.
  * 
- * @param {Element} element
- * @returns {Element|null}
+ * @private
+ * @param {HTMLElement} element 
  */
-export function getScrollableElement(element) {
-  if (element instanceof ShadowRoot) {
-    // Didn't find a scrollable ancestor.
-    return null;
-  }
+function isElementScrollable(element) {
   const style = getComputedStyle(element);
   const overflowX = style.overflowX;
   const overflowY = style.overflowY;
-  if (overflowX === 'scroll' || overflowX === 'auto' ||
-      overflowY === 'scroll' || overflowY === 'auto') {
-    // Found an element that can scroll.
-    return element;
+  return (overflowX === 'scroll' || overflowX === 'auto' ||
+      overflowY === 'scroll' || overflowY === 'auto');
+}
+
+
+/**
+ * If the given element can be scrolled, return that. If not, return the closest
+ * ancestor that can be scrolled. If no such ancestor is found, return null.
+ * 
+ * @param {Element} node
+ * @returns {Element|null}
+ */
+export function getScrollableElement(node) {
+  if (node instanceof HTMLElement && isElementScrollable(node)) {
+    return node;
+  }
+  for (const ancestor of composedAncestors(node)) {
+    if (ancestor instanceof HTMLElement && isElementScrollable(ancestor)) {
+      return ancestor;
+    }
+  }
+  return null;
+}
+
+
+export function willGestureScroll(target, orientation, delta) {
+  if (target instanceof ShadowRoot) {
+    // Didn't find a scrollable ancestor.
+    return false;
+  }
+  const style = getComputedStyle(target);
+  const vertical = orientation === 'vertical';
+  const scrollAxisMatch =
+    (vertical &&
+      (style.overflowY === 'scroll' || style.overflowY === 'auto')) ||
+    (!vertical &&
+      (style.overflowX === 'scroll' || style.overflowX === 'auto'));
+  if (scrollAxisMatch) {
+    // Found an target that can potentially scroll in this orientation.
+    const scrollEdge = vertical ? 'scrollTop' : 'scrollLeft';
+    if (delta < 0 && target[scrollEdge] > 0) {
+      // Target has room to scroll up or left.
+      return true;
+    }
+    const scrollLength = vertical ? 'scrollHeight' : 'scrollWidth';
+    const clientLength = vertical ? 'clientHeight' : 'clientWidth';
+    const scrollMax = target[scrollLength] + target[clientLength];
+    if (delta > 0 && target[scrollEdge] < scrollMax) {
+      // Target has room to scroll down or right.
+      return true;
+    }
   }
   // Keep looking higher in the hierarchy for a scrollable ancestor.
-  return element.parentNode instanceof Element ?
-    getScrollableElement(element.parentNode) :
-    null;
+
+  // TODO: Walk up shadow host
+  return target.parentNode instanceof Element ?
+    willGestureScroll(target.parentNode, orientation, delta) :
+    false;
 }
