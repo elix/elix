3cd30ba4df93328a179cce5fc2011cd2f50126f0
Jan Miksovsky <jan@miksovsky.com>
2019-11-08 14:26:25 -0800

    Work around a bug in ListComboBox and its variations that prevent the focus from staying on the combo box's input elements in browsers that support native delegatesFocus.

--- src/DelegateFocusMixin.js
+++ src/DelegateFocusMixin.js
@@ -62,10 +62,23 @@ export default function DelegateFocusMixin(Base) {
     }
 
     get [internal.focusTarget]() {
-      /** @type {any} */ const cast = this.shadowRoot;
-      return cast.delegatesFocus
-        ? this
-        : firstFocusableElement(this.shadowRoot);
+      // HACK: The commented-out code lets us rely on the browser to indicate
+      // which element should be focused on in browsers that don't support
+      // native delegatesFocus. However, this code creates subtle focus problems
+      // in components like AutoCompleteListBox: if the user clicks the toggle
+      // button, the focus won't be placed on the top-level AutoCompleteComboBox
+      // as expected; that element will be returned as the focus target, but if
+      // it doesn't have a non-negative tabindex, forwardFocus won't think it's
+      // focusable. A more correct solution would be for all components that are
+      // focusable to give themselves a tabIndex of 0 by default. Until we have
+      // to fully explore that, we workaround the bug by providing the polyfill
+      // behavior even in browsers that have delegatesFocus.
+
+      // /** @type {any} */ const cast = this.shadowRoot;
+      // return cast.delegatesFocus
+      //   ? this
+      //   : firstFocusableElement(this.shadowRoot);
+      return firstFocusableElement(this.shadowRoot);
     }
   }
 
