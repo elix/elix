9082d5edfa798240351e369eee55d50e1669aa19
Jan Miksovsky <jan@miksovsky.com>
2020-02-15 17:33:49 -0800

    Restore CarouselComboBox demo.

--- demos/src/CarouselComboBox.js
+++ demos/src/CarouselComboBox.js
@@ -1,7 +1,7 @@
 import * as internal from "../../src/base/internal.js";
 import * as template from "../../src/core/template.js";
-import AutoCompleteComboBox from "../../src/base/AutoCompleteComboBox.js";
-import Carousel from "../../src/base/Carousel.js";
+import AutoCompleteComboBox from "../../src/plain/AutoCompleteComboBox.js";
+import Carousel from "../../src/plain/Carousel.js";
 
 class CarouselComboBox extends AutoCompleteComboBox {
   get [internal.defaultState]() {
