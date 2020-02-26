e65673f53fca91ef22111e5944d7d3292ec9d41e
Jan Miksovsky <jmiksovsky@salesforce.com>
2020-02-26 15:16:48 -0800

    Add type declaration file for html utility.

---
+++ src/core/html.d.ts
@@ -0,0 +1,7 @@
+// Elix is a JavaScript project, but we define TypeScript declarations so we can
+// confirm our code is type safe, and to support TypeScript users.
+
+export default function html(
+  strings: TemplateStringsArray,
+  ...substitutions: string[]
+): DocumentFragment;
