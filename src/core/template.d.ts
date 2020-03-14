1d3aec31e0bf14953c505f27a4fb23e59e0f70a9
Jan Miksovsky <jan@miksovsky.com>
2020-03-14 15:38:24 -0700

    Tighten type declarations for transmute and createElement.

--- src/core/template.d.ts
+++ src/core/template.d.ts
@@ -1,13 +1,18 @@
 // Elix is a JavaScript project, but we define TypeScript declarations so we can
 // confirm our code is type safe, and to support TypeScript users.
 
-export function createElement(descriptor: PartDescriptor): Node;
+export function createElement(
+  descriptor: PartDescriptor
+): HTMLElement | SVGElement;
 export function html(
   strings: TemplateStringsArray,
   ...substitutions: any[]
 ): HTMLTemplateElement;
 export function replace(original: Node, replacement: Node): Node;
-export function transmute(original: Node, descriptor: PartDescriptor): Node;
+export function transmute(
+  original: HTMLElement | SVGElement,
+  descriptor: PartDescriptor
+): HTMLElement | SVGElement;
 export function wrap(
   original: Node,
   wrapper: DocumentFragment | Element,
