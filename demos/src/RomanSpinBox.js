4b22c197d5b4dd9a81385f64f1ec3e23669ba0b7
Jan Miksovsky <jan@miksovsky.com>
2020-03-23 17:43:23 -0700

    Fanciful Roman number spin box demo

---
+++ demos/src/RomanSpinBox.js
@@ -0,0 +1,61 @@
+import PlainSpinBox from "../../src/plain/PlainSpinBox.js";
+
+const romanToNumberMap = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1
+};
+
+export default class RomanSpinBox extends PlainSpinBox {
+  stepDown() {
+    const number = romanToNumber(this.value);
+    this.value = numberToRoman(number - 1);
+  }
+
+  stepUp() {
+    const number = romanToNumber(this.value);
+    this.value = numberToRoman(number + 1);
+  }
+}
+
+function numberToRoman(n) {
+  if (n < 1) {
+    return "";
+  }
+  let roman = "";
+  for (let i in romanToNumberMap) {
+    while (n >= romanToNumberMap[i]) {
+      roman += i;
+      n -= romanToNumberMap[i];
+    }
+  }
+  return roman;
+}
+
+function romanToNumber(roman) {
+  let n = 0;
+  for (let i = 0; i < roman.length; i++) {
+    const currentRoman = roman[i];
+    const currentNumber = romanToNumberMap[currentRoman];
+    const nextRoman = roman[i + 1];
+    const nextNumber = romanToNumberMap[nextRoman];
+    if (currentNumber < nextNumber) {
+      n -= currentNumber;
+    } else {
+      n += currentNumber;
+    }
+  }
+  return n;
+}
+
+customElements.define("units-spin-box", RomanSpinBox);
