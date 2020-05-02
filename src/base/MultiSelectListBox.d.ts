c3d4b9748a5710d21e3850be76b2f4aacd14c98a
Jan Miksovsky <jan@miksovsky.com>
2020-05-02 16:01:54 -0700

    Add TypeScript declaration files for components that were missing them.

---
+++ src/base/MultiSelectListBox.d.ts
@@ -0,0 +1,61 @@
+// Elix is a JavaScript project, but we define TypeScript declarations so we can
+// confirm our code is type safe, and to support TypeScript users.
+
+import ReactiveElement from "../core/ReactiveElement.js";
+import AriaListMixin from "./AriaListMixin.js";
+import ComposedFocusMixin from "./ComposedFocusMixin.js";
+import CurrentItemInViewMixin from "./CurrentItemInViewMixin.js";
+import CursorAPIMixin from "./CursorAPIMixin.js";
+import DirectionCursorMixin from "./DirectionCursorMixin.js";
+import FocusVisibleMixin from "./FocusVisibleMixin.js";
+import ItemsAPIMixin from "./ItemsAPIMixin.js";
+import ItemsCursorMixin from "./ItemsCursorMixin.js";
+import ItemsMultiSelectMixin from "./ItemsMultiSelectMixin.js";
+import ItemsTextMixin from "./ItemsTextMixin.js";
+import KeyboardDirectionMixin from "./KeyboardDirectionMixin.js";
+import KeyboardMixin from "./KeyboardMixin.js";
+import KeyboardPagedCursorMixin from "./KeyboardPagedCursorMixin.js";
+import KeyboardPrefixCursorMixin from "./KeyboardPrefixCursorMixin.js";
+import LanguageDirectionMixin from "./LanguageDirectionMixin.js";
+import MultiSelectAPIMixin from "./MultiSelectAPIMixin.js";
+import MultiSelectToggleMixin from "./MultiSelectToggleMixin.js";
+import SingleSelectAPIMixin from "./SingleSelectAPIMixin.js";
+import SlotItemsMixin from "./SlotItemsMixin.js";
+
+export default class MultiSelectListBox extends AriaListMixin(
+  ComposedFocusMixin(
+    CurrentItemInViewMixin(
+      CursorAPIMixin(
+        DirectionCursorMixin(
+          FocusVisibleMixin(
+            ItemsAPIMixin(
+              ItemsCursorMixin(
+                ItemsMultiSelectMixin(
+                  ItemsTextMixin(
+                    KeyboardDirectionMixin(
+                      KeyboardMixin(
+                        KeyboardPagedCursorMixin(
+                          KeyboardPrefixCursorMixin(
+                            LanguageDirectionMixin(
+                              MultiSelectAPIMixin(
+                                MultiSelectToggleMixin(
+                                  SingleSelectAPIMixin(
+                                    SlotItemsMixin(ReactiveElement)
+                                  )
+                                )
+                              )
+                            )
+                          )
+                        )
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+) {}
