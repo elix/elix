75bbfaffa6c25e83ab1d3cbd7f1821e3d48a3fd2
Lee White <leewhite@salesforce.com>
2020-09-24 13:46:57 -0700

    defined label, made plain version

---
+++ define/Label.js
@@ -0,0 +1,3 @@
+import PlainLabel from '../src/plain/PlainLabel.js';
+export default class ElixLabel extends PlainLabel {}
+customElements.define('elix-label', ElixLabel);
---
+++ src/plain/PlainLabel.js
@@ -0,0 +1,10 @@
+import Label from '../base/Label.js';
+
+/**
+ * Label component in the Plain reference design system
+ *
+ * @inherits Label
+ */
+class PlainLabel extends Label {}
+
+export default PlainLabel;
