b0ffecaa5677d537ea6df6029c213182a9acdfed
Lee White <leewhite@salesforce.com>
2020-09-24 15:28:31 -0700

    delegate for attr to label, passes for toinner

--- src/base/Label.js
+++ src/base/Label.js
@@ -1,5 +1,14 @@
 import { fragmentFrom } from '../core/htmlLiterals.js';
-import { firstRender, ids, render, template } from './internal.js';
+import {
+    defaultState,
+    firstRender,
+    ids,
+    render,
+    rendered,
+    setState,
+    state,
+    template
+} from './internal.js';
 import WrappedStandardElement from './WrappedStandardElement.js';
 
 const Base = WrappedStandardElement.wrap('label');
@@ -17,11 +26,63 @@ const Base = WrappedStandardElement.wrap('label');
  */
 
 class Label extends Base {
-    [render](/** @type {ChangedFlags} */ changed) {
+    get for() {
+        return this[state].for;
+    }
+
+    set for(HTMLFor) {
+        if (!this[state].removingAriaAttribute) {
+            this[setState]({
+                for: String(HTMLFor)
+            });
+        }
+    }
+
+    get [defaultState]() {
+        return Object.assign(super[defaultState] || {}, {
+            for: null,
+            removingAriaAttribute: false
+        });
+    }
+
+    [render](changed) {
         super[render](changed);
 
         if (this[firstRender]) {
             this[ids].inner.setAttribute('aria-hidden', 'true');
+            this[ids].inner.addEventListener('click', () => {});
+        }
+
+        if (changed.for) {
+            const HTMLFor = this[state].for;
+            if (HTMLFor) {
+                this[ids].inner.setAttribute('for', HTMLFor);
+            } else {
+                this[ids].inner.removeAttribute('for');
+            }
+        }
+    }
+
+    [rendered](changed) {
+        if (this[firstRender]) {
+            const HTMLFor = this.getAttribute('for');
+            this[setState]({ for: HTMLFor });
+        }
+        const HTMLFor = this[state].for;
+        if (changed.for && !this[state].removingAriaAttribute) {
+            if (this.getAttribute('for')) {
+                this.setAttribute('delegated-for', HTMLFor);
+                this[setState]({ removingAriaAttribute: true });
+                this.removeAttribute('for');
+            }
+        }
+
+        if (
+            changed.removingAriaAttribute &&
+            this[state].removingAriaAttribute
+        ) {
+            // We've done whatever removal we needed, and can now reset our flag.
+            this[setState]({ removingAriaAttribute: false });
         }
     }
 
