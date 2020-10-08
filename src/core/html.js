f1d4f4967708925ef962d95b0b2afb5ad62c62c2
Jan Miksovsky <jmiksovsky@salesforce.com>
2020-10-08 10:27:39 -0700

    Only generate console warning if dev actually invokes deprecated html template function in html.js. The previous warning was generated if the file was simply loaded, which happened if someone was using the top-level elix.js library file.

--- src/core/html.js
+++ src/core/html.js
@@ -6,9 +6,10 @@
 
 import { fragmentFrom } from "./htmlLiterals.js";
 
-export default fragmentFrom.html;
-
-/* eslint-disable no-console */
-console.warn(
-  `The html helper has moved to htmlLiterals.js, and is now called \`fragmentFrom.html\`. Please update your imports.`
-);
+export default function html(strings, ...substitutions) {
+  /* eslint-disable no-console */
+  console.warn(
+    `The html helper has moved to htmlLiterals.js, and is now called \`fragmentFrom.html\`. Please update your imports.`
+  );
+  return fragmentFrom.html(strings, ...substitutions);
+}
