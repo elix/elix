f5ca99a9678afeb32ed5dd5c2b26ee776ff6f852
Jan Miksovsky <jmiksovsky@salesforce.com>
2020-10-16 17:53:39 -0700

    Comments, clean up.

--- src/base/positionPopup.js
+++ src/base/positionPopup.js
@@ -1,169 +1,5 @@
-/**
- * Position the popup element with respect to a source element.
- *
- * @private
- * @param {HTMLElement} source
- * @param {HTMLElement} popup
- * @param {any} bounds
- * @param {any} options
- */
-export default function positionPopup(source, popup, bounds, options) {
-  const { popupAlign, popupDirection } = normalizeOptions(options);
-
-  // Given the direction and alignment, which layouts do we want to consider?
-  const layouts = prioritizedLayouts(popupDirection, popupAlign);
-  // Find the first layout that lets the popup fit in the bounds.
-  const bestLayout = layouts.find(({ align, direction }) => {
-    const sourceOrigin = getSourceOrigin(source, direction, align);
-    const { height, width } = availableSpace(
-      sourceOrigin,
-      bounds,
-      direction,
-      align
-    );
-    return popup.offsetHeight <= height && popup.offsetWidth <= width;
-  });
-  // If we didn't find any layout that works, take the first one.
-  const layout = bestLayout || layouts[0];
-  const { align, direction } = layout;
-
-  // With respect to which point on the source will we position the popup?
-  const sourceOrigin = getSourceOrigin(source, direction, align);
-
-  // Find the popup origin with respect to the source origin.
-  const { x, y } = getPopupOrigin(popup, sourceOrigin, direction, align);
-
-  // Return the (x, y) popup origin in (left, top) terms.
-  return {
-    left: x,
-    top: y,
-  };
-}
-
-function getPopupOrigin(popup, sourceOrigin, direction, align) {
-  // In the desired layout, which "corner" on the popup (could a real corner, or
-  // could be a midpoint) touches the source origin?
-  const layout = `${direction} ${align}`;
-  const mapLayoutToPopupCorner = {
-    "above left": "left bottom",
-    "above center": "center bottom",
-    "above right": "right bottom",
-    "below left": "left top",
-    "below center": "center top",
-    "below right": "right top",
-    "left top": "right top",
-    "left center": "right center",
-    "left bottom": "right bottom",
-    "right top": "left top",
-    "right center": "left center",
-    "right bottom": "left bottom",
-  };
-  const corner = mapLayoutToPopupCorner[layout];
-  const [cornerHorizontal, cornerVertical] = corner.split(" ");
-
-  // Starting at the source origin, and taking into account the popup's size,
-  // find the (x, y) position of the desired corner on the popup.
-  const height = popup.offsetHeight;
-  const width = popup.offsetWidth;
-  const x = {
-    center: sourceOrigin.x - width / 2,
-    left: sourceOrigin.x,
-    right: sourceOrigin.x - width,
-  }[cornerHorizontal];
-  const y = {
-    bottom: sourceOrigin.y - height,
-    center: sourceOrigin.y - height / 2,
-    top: sourceOrigin.y,
-  }[cornerVertical];
-
-  return { x, y };
-}
-
-function prioritizedLayouts(direction, align) {
-  const possibilties = [{ align, direction }];
-  const flipDirection = {
-    above: "below",
-    below: "above",
-    left: "right",
-    right: "left",
-  };
-  const flipAlign = {
-    top: "bottom",
-    bottom: "top",
-    left: "right",
-    right: "left",
-  };
-  if (align === "center") {
-    // Only need to consider possibility of flipping over main axis
-    possibilties.push({
-      align,
-      direction: flipDirection[direction],
-    });
-  } else {
-    // Need to consider possibilities of flipping on either axis or both
-    possibilties.push({
-      align: flipAlign[align],
-      direction: direction,
-    });
-    possibilties.push({
-      align,
-      direction: flipDirection[direction],
-    });
-    possibilties.push({
-      align: flipAlign[align],
-      direction: flipDirection[direction],
-    });
-  }
-  return possibilties;
-}
-
-/**
- * @private
- * @param {HTMLElement} source
- * @param {*} direction
- * @param {*} align
- */
-function getSourceOrigin(source, direction, align) {
-  let x;
-  let y;
-  switch (direction) {
-    case "above":
-      y = source.offsetTop;
-      break;
-    case "below":
-      y = source.offsetTop + source.offsetHeight;
-      break;
-    case "left":
-      x = source.offsetLeft;
-      break;
-    case "right":
-      x = source.offsetLeft + source.offsetWidth;
-      break;
-  }
-  switch (align) {
-    case "bottom":
-      y = source.offsetTop + source.offsetHeight;
-      break;
-    case "left":
-      x = source.offsetLeft;
-      break;
-    case "center":
-      if (direction === "above" || direction === "below") {
-        x = source.offsetLeft + source.offsetWidth / 2;
-      } else {
-        y = source.offsetTop + source.offsetHeight / 2;
-      }
-      break;
-    case "right":
-      x = source.offsetLeft + source.offsetWidth;
-      break;
-    case "top":
-      y = source.offsetTop;
-      break;
-  }
-  return { x, y };
-}
-
+// Given an (x, y) origin point, a bounding rectangle, and a layout, return the
+// height and width of the available space in the quadrant used by that layout.
 function availableSpace(origin, bounds, direction, align) {
   let height = 0;
   let width = 0;
@@ -207,9 +43,95 @@ function availableSpace(origin, bounds, direction, align) {
   return { height, width };
 }
 
-// Normalize the popup options. Convert any logical options (start, end) to
-// physical options (e.g., left, right). Replace any unknown option values with
-// defaults.
+// Determine the (x, y) location at which the popup should be positioned to
+// touch the indicated source origin point.
+function getPopupOrigin(popup, sourceOrigin, direction, align) {
+  let x;
+  let y;
+  switch (direction) {
+    case "above":
+      y = sourceOrigin.y - popup.offsetHeight;
+      break;
+    case "below":
+      y = sourceOrigin.y;
+      break;
+    case "left":
+      x = sourceOrigin.x - popup.offsetWidth;
+      break;
+    case "right":
+      x = sourceOrigin.x;
+      break;
+  }
+  switch (align) {
+    case "bottom":
+      y = sourceOrigin.y - popup.offsetHeight;
+      break;
+    case "left":
+      x = sourceOrigin.x;
+      break;
+    case "center":
+      if (direction === "above" || direction === "below") {
+        x = sourceOrigin.x - popup.offsetWidth / 2;
+      } else {
+        y = sourceOrigin.y - popup.offsetHeight / 2;
+      }
+      break;
+    case "right":
+      x = sourceOrigin.x - popup.offsetWidth;
+      break;
+    case "top":
+      y = sourceOrigin.y;
+      break;
+  }
+  return { x, y };
+}
+
+// For a given layout, we will use a different point on the source element as a
+// reference point to position the popup. Return that (x, y) point.
+function getSourceOrigin(/** @type {HTMLElement} */ source, direction, align) {
+  let x = 0;
+  let y = 0;
+  switch (direction) {
+    case "above":
+      y = source.offsetTop;
+      break;
+    case "below":
+      y = source.offsetTop + source.offsetHeight;
+      break;
+    case "left":
+      x = source.offsetLeft;
+      break;
+    case "right":
+      x = source.offsetLeft + source.offsetWidth;
+      break;
+  }
+  switch (align) {
+    case "bottom":
+      y = source.offsetTop + source.offsetHeight;
+      break;
+    case "left":
+      x = source.offsetLeft;
+      break;
+    case "center":
+      if (direction === "above" || direction === "below") {
+        x = source.offsetLeft + source.offsetWidth / 2;
+      } else {
+        y = source.offsetTop + source.offsetHeight / 2;
+      }
+      break;
+    case "right":
+      x = source.offsetLeft + source.offsetWidth;
+      break;
+    case "top":
+      y = source.offsetTop;
+      break;
+  }
+  return { x, y };
+}
+
+// Normalize the popup options. Convert any logical layout options (start, end)
+// to physical options (e.g., left, right). Replace any unknown option values
+// with defaults.
 function normalizeOptions(options) {
   const {
     popupAlign: logicalAlign,
@@ -260,4 +182,96 @@ function normalizeOptions(options) {
     popupDirection: physicalDirection,
     rightToLeft,
   };
+}
+
+/**
+ * Position the popup element with respect to a source element.
+ *
+ * @private
+ * @param {HTMLElement} source
+ * @param {HTMLElement} popup
+ * @param {any} bounds
+ * @param {any} options
+ */
+export default function positionPopup(source, popup, bounds, options) {
+  const { popupAlign, popupDirection } = normalizeOptions(options);
+
+  // Given the direction and alignment, which layouts do we want to consider?
+  const layouts = prioritizedLayouts(popupDirection, popupAlign);
+  // Find the first layout that lets the popup fit in the bounds.
+  const bestLayout = layouts.find(({ align, direction }) => {
+    const sourceOrigin = getSourceOrigin(source, direction, align);
+    const { height, width } = availableSpace(
+      sourceOrigin,
+      bounds,
+      direction,
+      align
+    );
+    return popup.offsetHeight <= height && popup.offsetWidth <= width;
+  });
+  // If we didn't find any layout that works, take the first one.
+  const layout = bestLayout || layouts[0];
+  const { align, direction } = layout;
+
+  // With respect to which point on the source will we position the popup?
+  const sourceOrigin = getSourceOrigin(source, direction, align);
+
+  // Find the popup origin with respect to the source origin.
+  const { x, y } = getPopupOrigin(popup, sourceOrigin, direction, align);
+
+  // Return the (x, y) popup origin in (left, top) terms.
+  return {
+    left: x,
+    top: y,
+  };
+}
+
+/**
+ * Given a preferred direction and alignment, determine the set of 2 or 4 layout
+ * alternatives that should be considered, in priority order.
+ *
+ * @private
+ */
+function prioritizedLayouts(preferredDirection, preferredAlign) {
+  const flipDirection = {
+    above: "below",
+    below: "above",
+    left: "right",
+    right: "left",
+  };
+  const flipAlign = {
+    top: "bottom",
+    bottom: "top",
+    left: "right",
+    right: "left",
+  };
+
+  // Our first choice of layout will be the preferred options.
+  const possibilties = [
+    { align: preferredAlign, direction: preferredDirection },
+  ];
+
+  if (preferredAlign === "center") {
+    // Center align only needs to consider flipping over main axis.
+    possibilties.push({
+      align: preferredAlign,
+      direction: flipDirection[preferredDirection],
+    });
+  } else {
+    // Consider possibilities of flipping on either axis or both.
+    possibilties.push({
+      align: flipAlign[preferredAlign],
+      direction: preferredDirection,
+    });
+    possibilties.push({
+      align: preferredAlign,
+      direction: flipDirection[preferredDirection],
+    });
+    possibilties.push({
+      align: flipAlign[preferredAlign],
+      direction: flipDirection[preferredDirection],
+    });
+  }
+
+  return possibilties;
 }
