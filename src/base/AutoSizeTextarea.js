elix/elix

commit c22d2870a4658292bdba03cc93906380ece52013

Simplify how we track selection state.

src/base/AutoSizeTextarea.js
@@ -109,7 +109,10 @@ class AutoSizeTextarea extends Base {
         this[setState]({ valueTracksContent: false });
         /** @type {any} */
         const inner = this[ids].inner;
+        // Setting value implies updating selection state as well.
         this[setState]({
+          selectionEnd: inner.selectionEnd,
+          selectionStart: inner.selectionStart,
           value: inner.value,
         });
         this[raiseChangeEvents] = false;

src/base/Input.js
@@ -65,8 +65,14 @@ class Input extends Base {
        */
       this[ids].inner.addEventListener("input", () => {
         this[raiseChangeEvents] = true;
-        // Invoke the value setter to fix up selectionStart/selectionEnd too.
-        this.value = /** @type {any} */ (this.inner).value;
+        /** @type {any} */
+        const inner = this[ids].inner;
+        this.value = inner.value;
+        // Setting value implies updating selection state as well.
+        this[setState]({
+          selectionEnd: inner.selectionEnd,
+          selectionStart: inner.selectionStart,
+        });
         this[raiseChangeEvents] = false;
       });
     }

src/base/TrackTextSelectionMixin.js
@@ -4,10 +4,7 @@ import {
   firstRender,
   raiseChangeEvents,
   render,
-  rendered,
   setState,
-  state,
-  stateEffects,
 } from "./internal.js";
 
 /**
@@ -33,8 +30,8 @@ export default function TrackTextSelectionMixin(Base) {
     // @ts-ignore
     get [defaultState]() {
       return Object.assign(super[defaultState] || {}, {
-        selectionEnd: null,
-        selectionStart: null,
+        selectionEnd: 0,
+        selectionStart: 0,
       });
     }
 
@@ -46,12 +43,12 @@ export default function TrackTextSelectionMixin(Base) {
       if (this[firstRender]) {
         // The user can manually update the selection with the keyboard or
         // mouse. We listen to keydown and mousedown events, wait for the
-        // browser to perform its default action, and then check refresh our
-        // selection state in case it changed.
+        // browser to perform its default action, and then refresh the selection
+        // state in case it changed.
         const refreshListener = (() => {
           // HACK:  If we check too quickly, the default action won't have
           // happened. We wait for an arbitrary amount of time that seems to
-          // work, but that feels gross.
+          // work, although this feels gross.
           const delay = 10; // milliseconds
           setTimeout(() => {
             this[raiseChangeEvents] = true;
@@ -62,42 +59,6 @@ export default function TrackTextSelectionMixin(Base) {
         this.addEventListener("keydown", refreshListener);
         this.addEventListener("mousedown", refreshListener);
       }
-    }
-
-    [rendered](changed) {
-      if (super[rendered]) {
-        super[rendered](changed);
-      }
-
-      // If either selection property is null, pick up its rendered value now.
-      const { selectionEnd, selectionStart } = this[state];
-      if (selectionEnd === null) {
-        this[setState]({
-          selectionEnd: this.inner.selectionEnd,
-        });
-      }
-      if (selectionStart === null) {
-        this[setState]({
-          selectionStart: this.inner.selectionStart,
-        });
-      }
-    }
-
-    [stateEffects](state, changed) {
-      const effects = super[stateEffects]
-        ? super[stateEffects](state, changed)
-        : {};
-
-      // Setting the value will implicitly update the selection. Clear out the
-      // selection state so it'll be refreshed after we render.
-      if (changed.value) {
-        Object.assign(effects, {
-          selectionStart: null,
-          selectionEnd: null,
-        });
-      }
-
-      return effects;
     }
   }
 

commit f1d8a321ff1c30e5fd0b564c2ca7803fd96db2c5

Small but potentially significant change in timing of when we set the raiseChangeEventsInNextRenderKey flag to false. Needed in order to refactor selection-tracking code in Input/AutoSizeTextarea in order to restore auto-complete behavior in AutoCompleteInput that was broken by c22d2870.

src/base/AutoSizeTextarea.js
@@ -106,14 +106,11 @@ class AutoSizeTextarea extends Base {
        */
       this[ids].inner.addEventListener("input", () => {
         this[raiseChangeEvents] = true;
-        this[setState]({ valueTracksContent: false });
         /** @type {any} */
         const inner = this[ids].inner;
-        // Setting value implies updating selection state as well.
         this[setState]({
-          selectionEnd: inner.selectionEnd,
-          selectionStart: inner.selectionStart,
           value: inner.value,
+          valueTracksContent: false,
         });
         this[raiseChangeEvents] = false;
       });

src/base/Input.js
@@ -10,6 +10,7 @@ import {
   raiseChangeEvents,
   render,
   setState,
+  state,
   template,
 } from "./internal.js";
 import TrackTextSelectionMixin from "./TrackTextSelectionMixin.js";
@@ -40,7 +41,7 @@ class Input extends Base {
   // @ts-ignore
   get [defaultState]() {
     return Object.assign(super[defaultState], {
-      valueCopy: "",
+      value: "",
     });
   }
 
@@ -67,12 +68,7 @@ class Input extends Base {
         this[raiseChangeEvents] = true;
         /** @type {any} */
         const inner = this[ids].inner;
-        this.value = inner.value;
-        // Setting value implies updating selection state as well.
-        this[setState]({
-          selectionEnd: inner.selectionEnd,
-          selectionStart: inner.selectionStart,
-        });
+        this[setState]({ value: inner.value });
         this[raiseChangeEvents] = false;
       });
     }
@@ -93,12 +89,12 @@ class Input extends Base {
   }
 
   get value() {
-    return super.value;
+    return this[state].value;
   }
   set value(value) {
-    const s = String(value);
-    super.value = s;
-    this[setState]({ valueCopy: s });
+    this[setState]({
+      value: String(value),
+    });
   }
 }
 

src/base/TrackTextSelectionMixin.js
@@ -4,7 +4,8 @@ import {
   firstRender,
   raiseChangeEvents,
   render,
-  setState
+  rendered,
+  setState,
 } from "./internal.js";
 
 /**
@@ -14,7 +15,7 @@ import {
  * intended to be used with a wrapped input or textarea. The inner input or
  * textarea will have selection properties `selectionStart` and `selectionEnd`
  * that we would like to track as state members. Doing so is challenging,
- * because the browser provides no standard event tracking a change in
+ * because the browser provides no standard event for tracking a change in
  * selection.
  *
  * To compensate, this mixin listens to keyboard or mouse activity within the
@@ -46,7 +47,7 @@ export default function TrackTextSelectionMixin(Base) {
         // browser to perform its default action, and then refresh the selection
         // state in case it changed.
         const refreshListener = (() => {
-          // HACK:  If we check too quickly, the default action won't have
+          // HACK: If we check too quickly, the default action won't have
           // happened. We wait for an arbitrary amount of time that seems to
           // work, although this feels gross.
           const delay = 10; // milliseconds
@@ -59,6 +60,15 @@ export default function TrackTextSelectionMixin(Base) {
         this.addEventListener("keydown", refreshListener);
         this.addEventListener("mousedown", refreshListener);
         this.addEventListener("touchend", refreshListener);
+      }
+    }
+
+    [rendered](changed) {
+      super[rendered](changed);
+
+      // Setting value implies updating selection state as well.
+      if (changed.value) {
+        refreshSelectionState(this);
       }
     }
   }

src/core/ReactiveMixin.js
@@ -150,6 +150,14 @@ export default function ReactiveMixin(Base) {
         const saveRaiseChangeEvents = this[raiseChangeEvents];
         this[raiseChangeEvents] = this[raiseChangeEventsInNextRenderKey];
 
+        // From this point on, we'll assume we won't need to raise events in the
+        // next render. If raiseChangeEvents is true right now, however, and the
+        // rendered method calls setState, then this flag will be set to true
+        // for the next render. That's apporopriate because the second-order
+        // setState call in rendered still counts as a user-initiated effect
+        // that should raise change events.
+        this[raiseChangeEventsInNextRenderKey] = false;
+
         // We set a flag to indicate that rendering is happening. The component
         // may use this to avoid triggering other updates during the render.
         this[rendering] = true;
@@ -170,9 +178,8 @@ export default function ReactiveMixin(Base) {
         // We've now rendered for the first time.
         this[firstRender] = false;
 
-        // Restore state of event flags.
+        // Restore state of event flag.
         this[raiseChangeEvents] = saveRaiseChangeEvents;
-        this[raiseChangeEventsInNextRenderKey] = saveRaiseChangeEvents;
       }
     }
 