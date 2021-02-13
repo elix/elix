fbaaade87f14e669cc3b5f8c7087a1374eb5bfc3
Jan Miksovsky <jan@miksovsky.com>
2021-02-13 13:52:32 -0800

    Styling

--- src/base/Drawer.js
+++ src/base/Drawer.js
@@ -448,9 +448,9 @@ class Drawer extends Base {
 
           #frameContent {
             display: block;
-            max-height: 100%;
-            max-width: 100%;
+            height: 100%;
             overflow: hidden;
+            width: 100%;
           }
 
           [part~="backdrop"] {
